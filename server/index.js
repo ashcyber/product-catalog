5707ef427db834203d4414b5c3eafdee08c477e0
Ash <ishans632@gmail.com>
2020-04-17 21:36:53 +0530

    configure nuxt with express

--- server/index.js
+++ server/index.js
@@ -3,18 +3,41 @@ require('./db/index')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const config = require('../nuxt.config.js')
 const productRoutes = require('./routes/product.routes')
-
-const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use('/api', productRoutes)
+// Express Config
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-app.use('/api', productRoutes)
+config.dev = process.env.NODE_ENV !== 'production'
 
-app.listen(PORT, () => {
-  console.log(`Listening to ${PORT}`)
-})
+async function start () {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  const { host, port } = nuxt.options.server
+
+  await nuxt.ready()
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
