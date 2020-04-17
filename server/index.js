4bf5f51950c318b1d882e45e62c7bb504ce75364
Ash <ishans632@gmail.com>
2020-04-17 22:20:56 +0530

    test-load data on frontend

--- server/index.js
+++ server/index.js
@@ -9,31 +9,27 @@ const config = require('../nuxt.config.js')
 const productRoutes = require('./routes/product.routes')
 const app = express()
 
-app.use('/api', productRoutes)
-// Express Config
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+app.use('/api', productRoutes)
 
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start () {
-  // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
   const { host, port } = nuxt.options.server
 
   await nuxt.ready()
-  // Build only in dev mode
+
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
 
-  // Give nuxt middleware to express
   app.use(nuxt.render)
 
-  // Listen the server
   app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
---
+++ services/ProductService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getProducts () {
+    return api.get('/products')
+  },
+  getProductById (id) {
+    return api.get(`/product/${id}`)
+  },
+  getProductBySlug (slug) {
+    return api.get(`/product/${slug}`)
+  }
+}
