5b708932201613d09a8015c9369acbfebfe935b6
Ash <ishans632@gmail.com>
2020-04-20 12:46:03 +0530

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -33,6 +33,7 @@ import { mapState } from 'vuex'
 import ProductCard from '~/components/ProductCard.vue'
 import FilterBar from '~/components/FilterBar.vue'
 import SearchBar from '~/components/SearchBar.vue'
+
 export default {
   components: {
     ProductCard,
